a918a41eefdb93389e24c2d270672f632aa2ea0a
Саид Мамаза <mamazaf90@gmail.com>
2024-03-04 13:45:35 +0600

    allowed hosts

--- doclab/settings.py
+++ doclab/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework',
     'drf_yasg',
+    'corsheaders',
     'apps.about_us',
     'apps.contacts',
     'apps.certificates',
@@ -62,7 +63,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+
+
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'doclab.urls'
 
@@ -135,6 +143,11 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+
+CORS_ALLOWED_ORIGINS = [
+    "https://salymbekov.pythonanywhere.com",
+    "https://salymbekov.pythonanywhere.com",
+]
 
 
 # Default primary key field type
