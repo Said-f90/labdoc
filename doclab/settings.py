a7c0c6d1fc42a2929d95916bbe9a2cd2902f486b
Саид Мамаза <mamazaf90@gmail.com>
2024-03-04 20:11:11 +0600

    allowed hosts

--- doclab/settings.py
+++ doclab/settings.py
@@ -150,9 +150,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "127.0.0.1:3000",
 ]
-
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",    
+]
 CORS_ALLOW_ALL_ORIGINS = True
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
